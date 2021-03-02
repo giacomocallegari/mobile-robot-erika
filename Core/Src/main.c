3a647bb7f6a754b4107331ec026c2d7781531c8e
Giacomo Callegari <giacomo.callegari@studenti.unitn.it>
2021-03-02 17:24:23 +0100

    Add timer initialization

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -207,11 +207,24 @@ void idle_hook(void) {
  * @return
  */
 int main(void) {
-  // Initialize the peripherals.
+  // Initialize the system.
   DemoHAL_Init();
 
+  // Initialize the peripherals.
+  MX_TIM3_Init();
+  MX_TIM6_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
+
+  // Start the PWM generation.
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+
+  // TODO: Initialize GPIO
+
+  HAL_Delay(1000);
 
   // Start the RTOS.
   StartOS(OSDEFAULTAPPMODE);
