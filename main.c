8e42339b68115ca92c91e889a7a68dc1e3dc1d5c
Giacomo Callegari <giacomo.callegari@studenti.unitn.it>
2021-03-02 08:45:38 +0100

    Define the task behavior and add supporting functions

--- main.c
+++ main.c
@@ -62,6 +62,53 @@ OsEE_addr volatile old_sp;
 uint32_t volatile idle_cnt;
 uint16_t volatile TaskControl_count;
 
+/* VARIABLES FOR THE ROBOT */
+
+int counterRelativeEncoder_M1 = 0;
+int counterRelativeEncoder_M1Old = 0;
+int counterRelativeEncoder_M2 = 0;
+int counterRelativeEncoder_M2Old = 0;
+int counterDiff_M1 = 0;
+int counterDiff_M2 = 0;
+
+float lowerMotorLimit = 2000;
+float upperMotorLimit = 10000;
+
+float estimatedSpeed_M1 = 0.0;
+float estimatedSpeed_M2 = 0.0;
+float value_M1 = 0.0;
+float value_M2 = 0.0;
+float value_old_M1 = 0.0;
+float value_old_M2 = 0.0;
+float error_M1 = 0.0;
+float error_M2 = 0.0;
+float error_old_M1 = 0.0;
+float error_old_M2 = 0.0;
+float integral_M1 = 0.0;
+float integral_M2 = 0.0;
+float derivative_M1 = 0.0;
+float derivative_M2 = 0.0;
+
+/////////////////////////////////////////////////////
+
+float omegaDes = 0.3;
+float velDes = 0.05;
+
+float velDes_M1 = 0.0;
+float velDes_M2 = 0.0;
+
+float tickToTheta = 360.0 / (26);
+float Rr = 210;
+float wheelRadius = 0.023 / 2.0;
+float rearTrack = 0.144;
+
+float fromDegSToRPM = 60.0 / 360.0;
+
+/* -------------------------- */
+
+void PWM_Set(uint32_t value, uint32_t Channel);
+void resetPWM();
+
 DeclareTask(TaskControl);
 extern void idle_hook(void);
 extern void StartupHook(void);
@@ -71,20 +118,17 @@ extern void StartupHook(void);
 #define TASK_STR  (P2CONST(uint8_t, AUTOMATIC, OS_APPL_DATA))"TaskControl\r\n"
 #define HAL_DELAY_MS  1000U
 
-void StartupHook(void)
-{
-	DemoHAL_SerialInit();
+void StartupHook(void) {
+  DemoHAL_SerialInit();
 }
 
-void serial_print(char const * msg)
-{
+void serial_print(char const * msg) {
   SuspendAllInterrupts();
-  DemoHAL_SerialWrite((uint8_t const *)msg, strlen(msg));
+  DemoHAL_SerialWrite((uint8_t const *) msg, strlen(msg));
   ResumeAllInterrupts();
 }
 
-void print_sp(TaskType tid, OsEE_addr sp)
-{
+void print_sp(TaskType tid, OsEE_addr sp) {
   static char msg[] = "TASK n SP<0xXXXX>\r\n  ";
   SuspendAllInterrupts();
   sprintf(msg, "TASK %d SP<%p>\r\n", tid, sp);
@@ -101,66 +145,191 @@ void print_sp(TaskType tid, OsEE_addr sp)
     }                                                               \
   } while ( 0 )
 
-void idle_hook(void)
-{
-	if ( !stk_wrong ) {
-		if (!old_sp) {
-			old_sp = osEE_get_SP();
-		}
-		else if (old_sp != osEE_get_SP()) {
-			stk_wrong = OSEE_TRUE;
-			OSEE_BREAK_POINT();
-		}
-	}
-
-	++idle_cnt;
-	if (idle_cnt >= IDLE_CNT_MAX) {
-		idle_cnt = 0;
-		ActivateTask(TaskControl);
-		DemoHAL_LedToggle(DEMO_HAL_LED_0);
-		serial_print(IDLE_STR);
-	}
-
-	DemoHAL_MainFunction();
-}
-
-int main(void)
-{
-	DemoHAL_Init();
-
-	DemoHAL_LedInit();
-
-	StartOS(OSDEFAULTAPPMODE);
-
-	return 0;
-}
-
-TASK(TaskControl)
-{
-  static OsEE_bool volatile stk_wrong = OSEE_FALSE;
-  static OsEE_addr volatile old_sp = 0;
-
-  serial_print(TASK_STR);
-
-  DemoHAL_LedOn(DEMO_HAL_LED_7);
-
-  DemoHAL_Delay(HAL_DELAY_MS);
-
-  DemoHAL_LedOff(DEMO_HAL_LED_7);
-
-  DemoHAL_Delay(HAL_DELAY_MS);
-
-  if ( !stk_wrong ) {
+void idle_hook(void) {
+  if (!stk_wrong) {
     if (!old_sp) {
       old_sp = osEE_get_SP();
-    }
-    else if (old_sp != osEE_get_SP()) {
+    } else if (old_sp != osEE_get_SP()) {
       stk_wrong = OSEE_TRUE;
-      OSEE_BREAK_POINT();
+      OSEE_BREAK_POINT()
+      ;
     }
   }
 
-  ++TaskControl_count;
+  ++idle_cnt;
+  if (idle_cnt >= IDLE_CNT_MAX) {
+    idle_cnt = 0;
+    ActivateTask(TaskControl);
+    DemoHAL_LedToggle(DEMO_HAL_LED_0);
+    serial_print(IDLE_STR);
+  }
+
+  DemoHAL_MainFunction();
+}
+
+/**
+ * Main function.
+ *
+ * @return
+ */
+int main(void) {
+  // Initialize the peripherals.
+  DemoHAL_Init();
+  DemoHAL_LedInit();
+
+  // Start the RTOS.
+  StartOS(OSDEFAULTAPPMODE);
+
+  return 0;
+}
+
+/**
+ * Task for the control of the robot.
+ */
+TASK(TaskControl) {
+  float dt = 0.02;
+
+  float kp = 0.06;
+  float ki = 0.00;
+  float kd = 0.01;
+
+  int initialTime = xTaskGetTickCount();
+
+  int decimator = 0;
+
+  float integral_M1 = 0.0;
+  float derivative_M1 = 0.0;
+
+  for (;;) {
+    if (decimator++ % 10 == 0) {
+      HAL_GPIO_TogglePin(LED_WHITE_GPIO_Port, LED_WHITE_Pin);
+    }
+
+    counterDiff_M1 = counterRelativeEncoder_M1 - counterRelativeEncoder_M1Old;
+    counterDiff_M2 = counterRelativeEncoder_M2 - counterRelativeEncoder_M2Old;
+    counterRelativeEncoder_M1Old = counterRelativeEncoder_M1;
+    counterRelativeEncoder_M2Old = counterRelativeEncoder_M2;
+    //Left estimated speed
+    estimatedSpeed_M1 = ((float) counterDiff_M1 / dt) * tickToTheta * fromDegSToRPM; //from delta_tick/s to rpm of the motor
+    //Right estimated speed
+    estimatedSpeed_M2 = -((float) counterDiff_M2 / dt) * tickToTheta * fromDegSToRPM; //from delta_tick/s to rpm of the motor
+
+    //From v and omega to omegaR and omegaL
+    float omegaR = 0.5 * (omegaDes * rearTrack + 2.0 * velDes) / wheelRadius;
+    float omegaL = 0.5 * (-omegaDes * rearTrack + 2.0 * velDes) / wheelRadius;
+
+    //From omegeR and omegaL to rmp M1 and rpm M2
+    velDes_M2 = omegaL * Rr * 60.0 / (2.0 * 3.14);
+    velDes_M1 = omegaR * Rr * 60.0 / (2.0 * 3.14);
+
+    //PID CONTROLLER M1
+    error_M1 = velDes_M1 - estimatedSpeed_M1;
+    integral_M1 = integral_M1 + error_M1 * dt;
+    derivative_M1 = (error_M1 - error_old_M1) / dt;
+    value_M1 = value_old_M1 + kp * (error_M1) + ki * integral_M1 + kd * derivative_M1;
+
+    //PID CONTROLLER M2
+    error_M2 = velDes_M2 - estimatedSpeed_M2;
+    integral_M2 = integral_M2 + error_M2 * dt;
+    derivative_M2 = (error_M2 - error_old_M2) / dt;
+    value_M2 = value_old_M2 + kp * (error_M2) + ki * integral_M2 + kd * derivative_M2;
+
+    float signM1 = 1.0;
+    float signM2 = 1.0;
+    if (velDes_M1 > 0) {
+      signM1 = 1.0;
+    } else {
+      signM1 = -1.0;
+    }
+    if (velDes_M2 > 0) {
+      signM2 = 1.0;
+    } else {
+      signM2 = -1.0;
+    }
+
+    //SATURATION OF PWM
+    if (fabs(value_M1) > upperMotorLimit) {
+      value_M1 = upperMotorLimit;
+    } else if (fabs(value_M1) < lowerMotorLimit) {
+      value_M1 = lowerMotorLimit;
+    }
+    if (fabs(value_M2) > upperMotorLimit) {
+      value_M2 = upperMotorLimit;
+    } else if (fabs(value_M2) < lowerMotorLimit) {
+      value_M2 = lowerMotorLimit;
+    }
+    value_M1 = fabs(value_M1) * signM1;
+    value_M2 = fabs(value_M2) * signM2;
+
+    if (value_M1 > 0) {
+      PWM_Set((uint32_t) (fabs(value_M1)), TIM_CHANNEL_4);
+      PWM_Set(0, TIM_CHANNEL_3);
+    } else {
+      PWM_Set(0, TIM_CHANNEL_4);
+      PWM_Set((uint32_t) (fabs(value_M1)), TIM_CHANNEL_3);
+    }
+
+    if (value_M2 > 0) {
+      PWM_Set((uint32_t) (fabs(value_M2)), TIM_CHANNEL_1);
+      PWM_Set(0, TIM_CHANNEL_2);
+    } else {
+      PWM_Set(0, TIM_CHANNEL_1);
+      PWM_Set((uint32_t) (fabs(value_M2)), TIM_CHANNEL_2);
+    }
+
+    error_old_M1 = error_M1;
+    value_old_M1 = value_M1;
+
+    error_old_M2 = error_M2;
+    value_old_M2 = value_M2;
+
+    osDelay(20);
+  }
 
   TerminateTask();
+}
+
+/**
+ * Resets the PWM variables.
+ */
+void resetPWM() {
+  error_M1 = 0;
+  error_M2 = 0;
+  integral_M1 = 0;
+  integral_M2 = 0;
+  derivative_M1 = 0;
+  derivative_M2 = 0;
+  error_old_M1 = 0;
+  error_old_M2 = 0;
+  value_M1 = 0;
+  value_M2 = 0;
+  value_old_M1 = 0;
+  value_old_M2 = 0;
+}
+
+/**
+ * Sets the PWM value on the selected channel.
+ *
+ * @param value     The PWM value
+ * @param Channel   The output channel
+ */
+void PWM_Set(uint32_t value, uint32_t Channel) {
+  switch (Channel) {
+  case TIM_CHANNEL_1: {
+    htim3.Instance->CCR1 = value;
+    break;
+  }
+  case TIM_CHANNEL_2: {
+    htim3.Instance->CCR2 = value;
+    break;
+  }
+  case TIM_CHANNEL_3: {
+    htim3.Instance->CCR3 = value;
+    break;
+  }
+  case TIM_CHANNEL_4: {
+    htim3.Instance->CCR4 = value;
+    break;
+  }
+  }
 }
